53d125006d26b1b33c139744e641d9bc7ed2e1a7
Johan van Soest <j.vansoest@maastrichtuniversity.nl>
2021-04-01 20:31:27 +0200

    Added the -ui option with tkinter

--- python/todoist_add.py
+++ python/todoist_add.py
@@ -5,11 +5,13 @@ import os
 from pathlib import Path
 import json
 import sys
+import tkinter as tk
 
 # Parse input parameters
 parser = argparse.ArgumentParser(description='Add task to Todoist Inbox.')
 parser.add_argument('-cb', '--clipboard', help='Read note contents from clipboard', action='store_true')
-parser.add_argument('taskName', help='the actual text for the task')
+parser.add_argument('-ui', '--userInterface', help='Show user interface to save information', action='store_true')
+parser.add_argument('taskName', nargs='?', default='', help='the actual text for the task')
 inputArgs = parser.parse_args()
 
 # Load ~/.cmd_tricks.json config file
@@ -25,11 +27,80 @@ if config is None:
 # Read Todoist API key form .cmd_tricks.json
 api = TodoistAPI(config["todoist"]["api_key"])
 
-taskName = inputArgs.taskName
-
-# Create task, if clipboard cmd is given, use this
+# get contents from clipboard
+cbContents = None
 if inputArgs.clipboard:
-    taskNote = clipboard.paste()
-    api.quick.add(taskName, note=taskNote)
+    cbContents = clipboard.paste()
+
+############################
+# Add GUI functions
+############################
+class UserInterface:
+
+    def __init__(self, todoistApi, taskName, taskNote):
+        # super().__init__()
+        self.todoistApi = todoistApi
+
+        root = tk.Tk()
+        root.title("Todoist quick entry")
+
+        self.txtTaskName = tk.Entry(root, width=80)
+        self.txtTaskName.pack(side=tk.TOP, anchor=tk.NW, pady=10, padx=10)
+        self.txtTaskName.insert(tk.INSERT, taskName)
+
+        self.txtNotes = tk.Text(root, width=60, height=12)
+        self.txtNotes.pack(side=tk.TOP, anchor=tk.NW, pady=10, padx=10)
+        if taskNote != None:
+            self.txtNotes.insert(tk.INSERT, taskNote)
+
+        btnSave = tk.Button(root, text="Save", command = self.saveActionButton)
+        btnSave.pack(side=tk.TOP)
+
+        # keybind control + enter to save action
+        root.bind('<Control-Return>', self.saveActionShortcut)
+
+        # keybind escape to exit
+        root.bind('<Escape>', self.quitAction)
+        self.txtTaskName.focus_set()
+
+        root.mainloop()
+
+    def saveActionButton(self):
+        taskName = self.txtTaskName.get()
+        taskNote = self.txtNotes.get("1.0",'end-1c')
+        self.todoistApi.addItemToTodoist(taskName, taskNote)
+
+    def saveActionShortcut(self, argument):
+        taskName = self.txtTaskName.get()
+        taskNote = self.txtNotes.get("1.0",'end-1c')
+        self.todoistApi.addItemToTodoist(taskName, taskNote)
+
+    def quitAction(self, argument):
+        sys.exit(0)
+        
+
+############################
+# Add actual todoist action
+############################
+class TodoistActions:
+    def __init__(self, api):
+        super().__init__()
+        self.api = api
+    def addItemToTodoist(self, taskName, taskNote):
+        savePerformed = False
+        if taskNote is not None:
+            if taskNote != "":
+                self.api.quick.add(taskName, note=taskNote)
+                savePerformed = True
+
+        if not savePerformed:
+            self.api.quick.add(taskName)
+        
+        sys.exit(0)
+
+# determine to show UI or not...
+todoistApi = TodoistActions(api)
+if inputArgs.userInterface:
+    UserInterface(todoistApi, inputArgs.taskName, cbContents)
 else:
-    api.quick.add(taskName)
+    todoistApi.addItemToTodoist(inputArgs.taskName, cbContents)
