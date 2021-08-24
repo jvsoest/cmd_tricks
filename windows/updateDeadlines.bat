5706cd796ccc09259abcafaa4419fff3ad64558d
Johan van Soest <j.vansoest@maastrichtuniversity.nl>
2021-08-24 11:59:16 +0200

    Updated the script to include upload to remote server via sftp

---
+++ windows/updateDeadlines.bat
@@ -0,0 +1,2 @@
+python "%USERPROFILE%\Documents\Repositories\cmd_tricks\python\todoist_deadlines.py" %*
+powershell echo \"put C:\Users\johan\OneDrive\deadlines.ics\" | sftp nuc10.fritz.box:/home/johan/Repositories/website/html/
