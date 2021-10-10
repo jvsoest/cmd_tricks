16320c07c828ee0ddb0bb7d41e55d78ec9563b40
Johan van Soest <j.vansoest@maastrichtuniversity.nl>
2021-10-10 21:08:50 +0200

    Added search for DL[yyyy-mm-dd] in title or text as actual deadline

--- python/todoist_deadlines.py
+++ python/todoist_deadlines.py
@@ -4,6 +4,7 @@ from pathlib import Path
 import json
 import sys
 from datetime import date
+import re
 
 # Load ~/.cmd_tricks.json config file
 config = None
@@ -32,12 +33,28 @@ class TodoistHelper:
 
 tdHelper = TodoistHelper(config["todoist"]["api_key"])
 
+def findDeadlineInString(stringToSearch):
+    deadlineString = None
+    searchResult = re.findall("DL[0-9]{4}-[0-9]{2}-[0-9]{2}", stringToSearch)
+    for deadlineString in searchResult:
+        deadlineString = deadlineString.replace("DL", "")
+    return deadlineString
+
 from icalendar import Calendar, Event
 cal = Calendar()
 for task in tdHelper.getDeadlineTasks():
+    
+    deadlineString = None
+    deadlineString = findDeadlineInString(task["content"])
+    if deadlineString is None:
+        deadlineString = findDeadlineInString(task["description"])
+
     event = Event()
     event.add('summary', task['content'])
-    event.add('dtstart', date.fromisoformat(task['due']['date']))
+    if deadlineString is not None:
+        event.add('dtstart', date.fromisoformat(deadlineString))
+    else:
+        event.add('dtstart', date.fromisoformat(task['due']['date']))
     event.add('description', task['url'])
     event.add('location', task['url'])
     cal.add_component(event)
