5b93002400db19d41484e1c81695e49a5a337f90
Johan van Soest <j.vansoest@maastrichtuniversity.nl>
2022-01-04 08:06:35 +0100

    dailyLog windowstyle hidden

--- windows/dailyLog.bat
+++ windows/dailyLog.bat
@@ -1,1 +1,1 @@
-powershell "code -n $HOME\OneDrive\Grip\; code $HOME/OneDrive/Grip/DailyLogs/$((Get-Date).tostring(\"yyyy_MM_dd\")).md"
+powershell -windowstyle hidden "code -n $HOME\OneDrive\Grip\; code $HOME/OneDrive/Grip/DailyLogs/$((Get-Date).tostring(\"yyyy_MM_dd\")).md"
