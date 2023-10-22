11cef1889f8ccb8ec2b1cc7c18507a6233d27623
Johan van Soest <j.vansoest@maastrichtuniversity.nl>
2023-10-22 19:29:46 +0200

    Added override to allow VBA macros in outlook

--- windows/forceOutlookCache.ps1
+++ windows/forceOutlookCache.ps1
@@ -1,3 +1,10 @@
 # See instructions at https://www.thewindowsclub.com/change-registry-using-windows-powershell
 # NOTE: THIS SCRIPT NEEDS TO BE EXECUTED WITH UAC/ADMIN PRIVILEGES
+
+# This override makes it possible to use the exchange cached mode in Outlook
 Set-ItemProperty -Path "HKCU:\SOFTWARE\Policies\Microsoft\office\16.0\outlook\cached mode" -Name enable -Value 1 -Force
+
+# This override makes it possible to use VBA macros in Outlook. Usual GPO value is 4 (allow none) where
+#   this override value (2) indicates that scripts are allowed but all scripts are notified beforehand.
+#   Usually this is setup in Outlook -> File -> Options -> Trust Center
+Set-ItemProperty -Path "HKCU:\SOFTWARE\Policies\Microsoft\office\16.0\outlook\security" -Name level -Value 2 -Force
