695d01589cea4cadfa2eac3812bac5fd88fb7586
Johan van Soest <j.vansoest@maastrichtuniversity.nl>
2023-11-24 20:09:25 +0100

    Override VBA in word (e.g. for Zotero functionality)

--- windows/forceOutlookCache.ps1
+++ windows/forceOutlookCache.ps1
@@ -8,3 +8,6 @@ Set-ItemProperty -Path "HKCU:\SOFTWARE\Policies\Microsoft\office\16.0\outlook\ca
 #   this override value (2) indicates that scripts are allowed but all scripts are notified beforehand.
 #   Usually this is setup in Outlook -> File -> Options -> Trust Center
 Set-ItemProperty -Path "HKCU:\SOFTWARE\Policies\Microsoft\office\16.0\outlook\security" -Name level -Value 2 -Force
+
+# This override makes it possible to use VBA macros in Word. Usual GPO value is 4 (blocked). This causes Zotero to not work properly.
+Set-ItemProperty -Path "HKCU:\SOFTWARE\Policies\Microsoft\office\16.0\word\security" -Name vbawarnings 1 -Force
