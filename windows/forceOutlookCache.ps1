304c66c01b99f1842912ee252ef06ac000c8cfa1
Johan van Soest <j.vansoest@maastrichtuniversity.nl>
2024-04-16 16:14:59 +0200

    Updated to use w11 file-context menu

--- windows/forceOutlookCache.ps1
+++ windows/forceOutlookCache.ps1
@@ -2,6 +2,7 @@
 # NOTE: THIS SCRIPT NEEDS TO BE EXECUTED WITH UAC/ADMIN PRIVILEGES
 
 # This override makes it possible to use the exchange cached mode in Outlook
+# Note: you might need to add the folder "cached mode" and the DWORD item "enable" manually
 Set-ItemProperty -Path "HKCU:\SOFTWARE\Policies\Microsoft\office\16.0\outlook\cached mode" -Name enable -Value 1 -Force
 
 # This override makes it possible to use VBA macros in Outlook. Usual GPO value is 4 (allow none) where
@@ -11,3 +12,8 @@ Set-ItemProperty -Path "HKCU:\SOFTWARE\Policies\Microsoft\office\16.0\outlook\se
 
 # This override makes it possible to use VBA macros in Word. Usual GPO value is 4 (blocked). This causes Zotero to not work properly.
 Set-ItemProperty -Path "HKCU:\SOFTWARE\Policies\Microsoft\office\16.0\word\security" -Name vbawarnings 1 -Force
+
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Appx" -Name AllowDeploymentInSpecialProfiles 1 -Force
+
+# Remove group policy that forces the old windows context menu
+reg.exe delete "HKCU\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}" /f
