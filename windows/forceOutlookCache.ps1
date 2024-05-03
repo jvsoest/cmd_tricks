253792101bc86908cc4267804eb5c7bfab174793
Johan van Soest <j.vansoest@maastrichtuniversity.nl>
2024-05-03 11:03:58 +0200

    Add option to use biometrics to login

--- windows/forceOutlookCache.ps1
+++ windows/forceOutlookCache.ps1
@@ -17,3 +17,6 @@ Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Appx" -Name Al
 
 # Remove group policy that forces the old windows context menu
 reg.exe delete "HKCU\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}" /f
+
+# Set PIN allowed (to enable biometrics as well)
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\System" -Name AllowDomainPINLogon -Value 1 -Force
