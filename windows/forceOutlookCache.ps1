bb426391998bbe3eb9610234a5cfa744d9b4aa5b
Johan van Soest <j.vansoest@maastrichtuniversity.nl>
2026-01-22 08:23:57 +0100

    not completely sure if this works, but explorer should start in recent locations

--- windows/forceOutlookCache.ps1
+++ windows/forceOutlookCache.ps1
@@ -23,3 +23,7 @@ reg.exe delete "HKCU\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a
 
 # Set PIN allowed (to enable biometrics as well)
 #Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\System" -Name AllowDomainPINLogon -Value 1 -Force
+
+# Force explorer.exe to open in recent location instead of loading network locations
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name Launchto -Value 2 -Force
+
